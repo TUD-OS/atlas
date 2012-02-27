6a0ab2c64204c2ba50b2bf92728821589e0c6960
Michael Roitzsch <mroi@os.inf.tu-dresden.de>
2012-02-27 18:46:00 +0000

    first official experiment since makeover: simple decoding time measurement

---
+++ Experiments/01 Decoding Times/Components/time.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2006-2012 Michael Roitzsch <mroi@os.inf.tu-dresden.de>
+ * economic rights: Technische Universitaet Dresden (Germany)
+ */
+
+#include <sys/time.h>
+
+#include "process.h"
+
+
+static void measure_times(AVCodecContext *c);
+
+static inline double get_time(void)
+{
+#ifdef __linux__
+	struct timespec ts;
+	clock_gettime(CLOCK_THREAD_CPUTIME_ID, &ts);
+	return (double)ts.tv_sec + (double)ts.tv_nsec / 1000000000.0;
+#else
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (double)tv.tv_sec + (double)tv.tv_usec / 1000000.0;
+#endif
+}
+
+
+void process_init(AVCodecContext *c, const char *file)
+{
+	c->process_slice = (void (*)(void *))measure_times;
+}
+
+void process_finish(AVCodecContext *c)
+{
+}
+
+static void measure_times(AVCodecContext *c)
+{
+	static double time;
+	
+	if (c->metrics.type == PSEUDO_SLICE_FRAME_START)
+		time = get_time();
+	if (c->metrics.type == PSEUDO_SLICE_FRAME_END)
+		printf("%lf\n", get_time() - time);
+}
