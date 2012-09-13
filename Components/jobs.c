a766480c112cee01d301b7424ad1450c060b9900
Michael Roitzsch <mroi@os.inf.tu-dresden.de>
2012-09-13 12:32:31 +0000

    factor out scratchpad read/write code and remember deadline and predicted time for later evaluation

--- Components/jobs.c
+++ Components/jobs.c
@@ -28,20 +28,42 @@ struct estimator_s {
 	pthread_mutex_t lock;
 	void *code;
 	pid_t tid;
+	
 	double time;
 	double previous_deadline;
-	llsp_t *llsp;
 	unsigned metrics_count;
-	unsigned size;
-	double *read_pos;
-	double *write_pos;
-	double metrics[];
+	llsp_t *llsp;
+	
+	struct scratchpad {
+		unsigned size;
+		double *read;
+		double *write;
+		double ringbuffer[];
+	} scratchpad;
 };
 
 static const unsigned initial_metrics_size = 256;
 
 static pthread_mutex_t estimator_enqueue = PTHREAD_MUTEX_INITIALIZER;
 static struct estimator_s *estimator_list = NULL;
+
+static inline void scratchpad_write(struct scratchpad *scratchpad, double value)
+{
+	*scratchpad->write = value;
+	if (++scratchpad->write > scratchpad->ringbuffer + scratchpad->size)
+		scratchpad->write = scratchpad->ringbuffer;  // ring buffer wrap around
+	assert(scratchpad->write != scratchpad->read);  // FIXME: realloc when full
+}
+
+static inline double scratchpad_read(struct scratchpad *scratchpad)
+{
+	double value = *scratchpad->read;
+	if (++scratchpad->read > scratchpad->ringbuffer + scratchpad->size)
+		scratchpad->read = scratchpad->ringbuffer;  // ring buffer wrap around
+	return value;
+}
+
+#pragma mark -
 
 
 #pragma mark Job Queue Management
@@ -60,11 +82,11 @@ void atlas_job_queue_checkin(void *code)
 		estimator->tid = 0;
 		estimator->time = 0.0;
 		estimator->previous_deadline = 0.0;
-		estimator->llsp = NULL;
 		estimator->metrics_count = 0;
-		estimator->size = initial_metrics_size;
-		estimator->read_pos = estimator->metrics;
-		estimator->write_pos = estimator->metrics;
+		estimator->llsp = NULL;
+		estimator->scratchpad.size = initial_metrics_size;
+		estimator->scratchpad.read = estimator->scratchpad.ringbuffer;
+		estimator->scratchpad.write = estimator->scratchpad.ringbuffer;
 		
 		pthread_mutex_lock(&estimator_enqueue);
 		estimator->next = estimator_list;
@@ -120,28 +142,25 @@ void atlas_job_submit_absolute(void *code, double deadline, unsigned count, cons
 	
 	pthread_mutex_lock(&estimator->lock);
 	
-	if (!estimator->llsp) {
-		estimator->metrics_count = count;
-		estimator->llsp = llsp_new(count);
-	}
-	
 	if (deadline < estimator->previous_deadline) {
 		printf("WARNING: deadlines not ordered (%lf < %lf)\n", deadline, estimator->previous_deadline);
 		deadline = estimator->previous_deadline;
 	}
 	estimator->previous_deadline = deadline;
 	
-	assert(estimator->metrics_count == count);
-	for (size_t i = 0; i < estimator->metrics_count; i++) {
-		*estimator->write_pos = metrics[i];
-		if (++estimator->write_pos > estimator->metrics + estimator->size)
-			estimator->write_pos = estimator->metrics;  // ring buffer wrap around
-		assert(estimator->write_pos != estimator->read_pos);  // FIXME: realloc when full
+	if (!estimator->llsp) {
+		estimator->metrics_count = count;
+		estimator->llsp = llsp_new(count);
 	}
-	
+	assert(estimator->metrics_count == count);
 	double prediction = 0.0;
 	if (llsp_solve(estimator->llsp))
 		prediction = llsp_predict(estimator->llsp, metrics);
+	
+	for (size_t i = 0; i < estimator->metrics_count; i++)
+		scratchpad_write(&estimator->scratchpad, metrics[i]);
+	scratchpad_write(&estimator->scratchpad, deadline);
+	scratchpad_write(&estimator->scratchpad, prediction);
 	
 	// FIXME: reasoning about deadline order is impossible when submitting relative deadlines
 	struct timeval tv;
@@ -159,9 +178,6 @@ void atlas_job_submit_absolute(void *code, double deadline, unsigned count, cons
 		.tv_usec = 1000000 * (prediction - (long long)prediction)
 	};
 	sched_submit(estimator->tid, &tv_exectime, &tv_deadline);
-	
-	static unsigned job_id = 0;
-	printf("job %u submitted: %lf, %lf\n", job_id++, deadline, prediction);
 	
 	pthread_mutex_unlock(&estimator->lock);
 }
@@ -196,20 +212,14 @@ void atlas_job_next(void *code)
 	
 	pthread_mutex_lock(&estimator->lock);
 	
-	if (estimator->read_pos != estimator->write_pos) {
+	if (estimator->scratchpad.read != estimator->scratchpad.write) {
 		double metrics[estimator->metrics_count];
-		for (size_t i = 0; i < estimator->metrics_count; i++) {
-			metrics[i] = *estimator->read_pos;
-			if (++estimator->read_pos > estimator->metrics + estimator->size)
-				estimator->read_pos = estimator->metrics;
-		}
+		for (size_t i = 0; i < estimator->metrics_count; i++)
+			metrics[i] = scratchpad_read(&estimator->scratchpad);
+		(void)scratchpad_read(&estimator->scratchpad);  // deadline
+		(void)scratchpad_read(&estimator->scratchpad);  // prediction
 		if (estimator->time > 0.0)
 			llsp_add(estimator->llsp, metrics, time - estimator->time);
-	}
-	
-	if (estimator->time > 0.0) {
-		static unsigned job_id = 0;
-		printf("job %u finished: %lf\n", job_id++, time - estimator->time);
 	}
 	estimator->time = time;
 	
