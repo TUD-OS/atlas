444ddadc8e632ade58abe5bbc71e5e4013217eca
Michael Roitzsch <mroi@os.inf.tu-dresden.de>
2012-10-14 14:37:05 +0000

    do not require include files when we import weak.c in an experiment

--- Components/weak.c
+++ Components/weak.c
@@ -3,25 +3,21 @@
  * economic rights: Technische Universitaet Dresden (Germany)
  */
 
-#include "jobs.h"
-#include "process.h"
-
-
 #ifdef __APPLE__
 
 /* The Darwin linker wants to have a default implementation for all weak symbols. This is not needed on Linux. */
-void hook_job_release(void *code) {}
-void hook_job_complete(void *code, double thread_time, double deadline, double prediction, double execution) {}
-void hook_slice_any(const AVCodecContext *c) {}
-void hook_slice_end(const AVCodecContext *c) {}
-void hook_frame_end(const AVCodecContext *c) {}
+void hook_job_release(void *code) __attribute__((weak)) {}
+void hook_job_complete(void *code, double thread_time, double deadline, double prediction, double execution) __attribute__((weak)) {}
+void hook_slice_any(const AVCodecContext *c) __attribute__((weak)) {}
+void hook_slice_end(const AVCodecContext *c) __attribute__((weak)) {}
+void hook_frame_end(const AVCodecContext *c) __attribute__((weak)) {}
 
-int frame_storage_alloc(AVCodecContext *c, AVFrame *frame)
+int frame_storage_alloc(AVCodecContext *c, AVFrame *frame) __attribute__((weak))
 {
 	return avcodec_default_get_buffer(c, frame);
 }
 
-void frame_storage_destroy(AVCodecContext *c, AVFrame *frame)
+void frame_storage_destroy(AVCodecContext *c, AVFrame *frame) __attribute__((weak))
 {
 	avcodec_default_release_buffer(c, frame);
 }
