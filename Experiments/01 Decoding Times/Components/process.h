dc1a8619f084e87e8148cd9b4314dc692240406c
Michael Roitzsch <mroi@os.inf.tu-dresden.de>
2012-10-15 14:47:44 +0000

    remove unneeded experiments

--- Experiments/01 Decoding Times/Components/process.h
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (C) 2006-2012 Michael Roitzsch <mroi@os.inf.tu-dresden.de>
- * economic rights: Technische Universitaet Dresden (Germany)
- */
-
-#include "libavcodec/avcodec.h"
-
-void process_init(AVCodecContext *c, const char *file);
-void process_finish(AVCodecContext *c);
--- Experiments/04 Preprocess/Components/replace.c
+++
@@ -1,1 +0,0 @@
-../../../Components/replace.c
