20d428eb173a7f4401cf624edb25b57896d80f2b
Michael Roitzsch <mroi@os.inf.tu-dresden.de>
2012-10-18 07:37:25 +0000

    • have the experiment time start with 0 • 10 concurrent CFS processes • proper clean rule

--- Experiments/02 CFS with Load/Components/experiment.c
+++ Experiments/02 CFS with Load/Components/experiment.c
@@ -13,12 +13,16 @@
 
 void hook_job_complete(void *code, double thread_time, double deadline, double prediction, double execution_time)
 {
+	static double start_time = 0.0;
+	static double previous_completion = 0.0;
+	
 	if (code == ffplay_stages[2].code) {
 		/* refresh stage */
-		static double previous_completion = 0.0;
 		double time = av_gettime() / 1000000.0;
+		if (start_time == 0.0)
+			start_time = time;
 		if (previous_completion > 0.0)
-			printf("%lf, %lf\n", time, time - previous_completion);
+			printf("%lf %lf\n", time - start_time, time - previous_completion);
 		previous_completion = time;
 	}
 }
