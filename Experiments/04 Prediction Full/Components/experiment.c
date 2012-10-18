a7c71565471911d60eb56c9a4c0d0b606c7f1060
Michael Roitzsch <mroi@os.inf.tu-dresden.de>
2012-10-18 07:38:15 +0000

    all experiments used in the paper

---
+++ Experiments/04 Prediction Full/Components/experiment.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2006-2012 Michael Roitzsch <mroi@os.inf.tu-dresden.de>
+ * economic rights: Technische Universitaet Dresden (Germany)
+ */
+
+#define IMPLEMENTS_HOOKS
+
+#include <stdio.h>
+
+#include "libavformat/avformat.h"
+#include "jobs.h"
+
+
+void hook_job_complete(void *code, double thread_time, double deadline, double prediction, double execution_time)
+{
+	if (code == ffplay_stages[1].code)
+		/* decoder stage */
+		printf("%lf %lf\n", prediction, execution_time);
+}
+
