1b122f69981d31b7b98fc3c748ddfb20c41a6118
Michael Roitzsch <mroi@os.inf.tu-dresden.de>
2013-03-16 14:47:58 +0000

    calibrate time stamp counter and convert timestamps to µsecs

--- Experiments/02 Times and Metrics/Components/metrics.c
+++ Experiments/02 Times and Metrics/Components/metrics.c
@@ -5,7 +5,11 @@
 
 #define IMPLEMENTS_HOOKS
 
-#include <inttypes.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <dispatch/dispatch.h>
+#include <libavutil/timer.h>
 #include "process.h"
 
 #if !FFMPEG_TIME || !FFMPEG_METRICS
@@ -15,27 +19,44 @@
 
 void hook_slice_any(const AVCodecContext *c)
 {
+	static dispatch_once_t predicate;
+	static float usecs_per_tsc = 0.0;
+	
+	dispatch_once(&predicate, ^{
+		struct timeval tv;
+		
+		gettimeofday(&tv, NULL);
+		double usecs_start = 1000000.0 * tv.tv_sec + (double)tv.tv_usec;
+		uint64_t tsc_start = read_time();
+		usleep(1000000);
+		uint64_t tsc_stop = read_time();
+		gettimeofday(&tv, NULL);
+		double usecs_stop = 1000000.0 * tv.tv_sec + (double)tv.tv_usec;
+		
+		usecs_per_tsc = (usecs_stop - usecs_start) / (tsc_stop - tsc_start);
+	});
+	
 	printf("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d, ",
-		   c->metrics.type,                              //  1
-		   (c->slice.end_index - c->slice.start_index),  //  2
-		   c->metrics.bits,                              //  3
-		   c->metrics.intra_pcm,                         //  4
-		   c->metrics.intra_4x4,                         //  5
-		   c->metrics.intra_8x8,                         //  6
-		   c->metrics.intra_16x16,                       //  7
-		   c->metrics.inter_4x4,                         //  8
-		   c->metrics.inter_8x8,                         //  9
-		   c->metrics.inter_16x16,                       // 10
-		   c->metrics.idct_4x4,                          // 11
-		   c->metrics.idct_8x8,                          // 12
-		   c->metrics.deblock_edges);                    // 13
-	printf("%" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", %" PRIu64 ", ",
-		   c->timing.decoder_prep,                       // 14
-		   c->timing.decompress_cabac,                   // 15
-		   c->timing.decompress_cavlc,                   // 16
-		   c->timing.spatial_pred,                       // 17
-		   c->timing.temporal_pred,                      // 18
-		   c->timing.idct,                               // 19
-		   c->timing.post);                              // 20
-	printf("%" PRIu64 "\n", c->timing.total);            // 21
+	       c->metrics.type,                              //  1
+	       (c->slice.end_index - c->slice.start_index),  //  2
+	       c->metrics.bits,                              //  3
+	       c->metrics.intra_pcm,                         //  4
+	       c->metrics.intra_4x4,                         //  5
+	       c->metrics.intra_8x8,                         //  6
+	       c->metrics.intra_16x16,                       //  7
+	       c->metrics.inter_4x4,                         //  8
+	       c->metrics.inter_8x8,                         //  9
+	       c->metrics.inter_16x16,                       // 10
+	       c->metrics.idct_4x4,                          // 11
+	       c->metrics.idct_8x8,                          // 12
+	       c->metrics.deblock_edges);                    // 13
+	printf("%f, %f, %f, %f, %f, %f, %f, ",
+	       usecs_per_tsc * c->timing.decoder_prep,       // 14
+	       usecs_per_tsc * c->timing.decompress_cabac,   // 15
+	       usecs_per_tsc * c->timing.decompress_cavlc,   // 16
+	       usecs_per_tsc * c->timing.spatial_pred,       // 17
+	       usecs_per_tsc * c->timing.temporal_pred,      // 18
+	       usecs_per_tsc * c->timing.idct,               // 19
+	       usecs_per_tsc * c->timing.post);              // 20
+	printf("%f\n", usecs_per_tsc * c->timing.total);     // 21
 }
