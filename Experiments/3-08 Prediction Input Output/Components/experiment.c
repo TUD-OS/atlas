06135297abf3ce156e3867fab4f8f20473e55961
Michael Roitzsch <mroi@os.inf.tu-dresden.de>
2013-05-29 16:56:49 +0200

    experiments for prediction accuracy
    • the base experiment is 3-05 (full-metrics prediction)
    • make it configurable with Makeconf.experiment
    • tie derived experiments to it via symlinks

---
+++ Experiments/3-08 Prediction Input Output/Components/experiment.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2006-2013 Michael Roitzsch <mroi@os.inf.tu-dresden.de>
+ * economic rights: Technische Universitaet Dresden (Germany)
+ */
+
+#define IMPLEMENTS_HOOKS
+
+#include <stdio.h>
+#include <string.h>
+
+#include "dispatch.h"
+#include "estimator.h"
+
+
+void hook_job_complete(void *code, double thread_time, double deadline, double prediction, double execution_time)
+{
+	(void)code;
+	(void)thread_time;
+	(void)deadline;
+	
+	dispatch_queue_t queue = dispatch_get_current_queue();
+	const char *label = dispatch_queue_get_label(queue);
+	
+	if (label && strcmp(label, "read") == 0)
+		/* input stage */
+		printf("0 %lf %lf\n", prediction, execution_time);
+	if (label && strcmp(label, "refresh") == 0)
+		/* output stage */
+		printf("1 %lf %lf\n", prediction, execution_time);
+}
